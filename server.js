03f7f7a48261fa5bbc19149acb1b80205deacc70
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-06-27 22:26:28 -0400

    updated file structure and directories

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var routes = require("./routes/routes");
+var apiController = require('./controllers/apiController')
+
+// Here we will set up the port, configure mongoose, configure our middleware
+var PORT = process.env.PORT || 3000;
+mongoose.Promise = bluebird;
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//app.use(express.static(__dirname + "/build"));
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/nyt-react";
+
+// Connect mongoose to our database
+mongoose.connect(db, function(error) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
